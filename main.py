2a993b3b200de56b8e08664523fd4224776889a9
Zhanna <nurbekovna@mail.ru>
2022-03-29 21:29:26 +0700

    3 tasks

--- main.py
+++ main.py
@@ -6,5 +6,14 @@ app = Flask(__name__)
 def index(title):
     return render_template('base.html', title=title)
 
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template('profs.html', prof=prof)
+
+@app.route('/list_prof/<param>')
+def list_prof(param):
+    return render_template('list.html', param=param)
+
+
 if __name__ == '__main__':
-    app.run(port=8000, host='127.0.0.1')
+    app.run(port=8000, host='127.0.0.3')
